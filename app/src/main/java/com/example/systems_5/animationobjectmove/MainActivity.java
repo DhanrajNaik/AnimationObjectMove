82bb13247e2455d5db2d94c5ca68e395650f46e2
DhanrajNaik <dhanrajnaik522@gmail.com>
2015-07-15 17:50:24 +0530

    Flip animation on x direction and Y direction

---
+++ app/src/main/java/com/example/systems_5/animationobjectmove/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.systems_5.animationobjectmove;
+
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    AnimatorSet set;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStart(){
+        super.onStart();
+        ImageView imageView = (ImageView) findViewById(R.id.imageview);
+        set = (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.flip);
+        set.setTarget(imageView);
+        set.start();
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
